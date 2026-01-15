f0b117484ab4ef6b119e1792376efb489bfa31d9
arishashaista <arishashaistaa@gmail.com>
2026-01-15 10:22:16 +0700

    refactor: use Ninja response schema for ping endpoint

--- merentalbe3/api.py
+++ merentalbe3/api.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from ninja import NinjaAPI
 
 api = NinjaAPI()
 
 
-@api.get("/ping")
+@api.get("/ping", response={200:str})
 def ping(request):
     """Simple ping endpoint returning plain text."""
-    return HttpResponse("pong", content_type="text/plain")
+    return "pong"
