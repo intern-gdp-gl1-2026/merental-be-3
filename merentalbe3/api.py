6bdc8a41e56a57c53cae2becbf827711fdc9069c
arishashaista <arishashaistaa@gmail.com>
2026-01-15 11:03:30 +0700

    refactor: spacing in response schema for PEP8 compliance

--- merentalbe3/api.py
+++ merentalbe3/api.py
@@ -3,7 +3,7 @@ from ninja import NinjaAPI
 api = NinjaAPI()
 
 
-@api.get("/ping", response={200:str})
+@api.get("/ping", response={200: str})
 def ping(request):
     """Simple ping endpoint returning plain text."""
     return "pong"
